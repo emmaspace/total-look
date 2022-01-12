22bb93bddc9a364a3610d5e5020f5ea1a5a5a4ec
Iliana00 <carrasquedo.279@gmail.com>
2022-01-12 10:30:12 -0600

    doing changes

--- src/Categories/AddCategory.js
+++ src/Categories/AddCategory.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
+import { EachCategory } from './EachCategory';
 import './Listcategories.css'
 import { ModalCategories } from './ModalCategories';
 
 
 export const AddCategory = () => {
     const [showModal, setShowModal] = useState(false)
+
+    const [category, setCategory] = useState('');
+    const [image, setImage] = useState('');
+    const [description, setDescription] = useState('')
     const openModal = () => {
         setShowModal(prev => !prev)
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("A new category has been added");
+      };
 
 
     return (
@@ -19,17 +29,35 @@ export const AddCategory = () => {
                 showModal={showModal}
                 setShowModal={setShowModal}
             >
-                <section className='addCatg'>
+                <form className='addCatg' onSubmit={handleSubmit}>
                     <p>Category</p>
-                    <input type="text" />
+                    <input                     
+                    type="text"                                         
+                    name="category"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    />
                     <p>Image</p>
-                    <input />
+                    <input 
+                    type="image"
+                    name='image'
+                    alt='an image represents a category'
+                    value={image}                    
+                    onChange={(e) => setImage(e.target.value)}
+                    />
                     <p>Description</p>
-                    <input type="text" />                    
-                </section>
+                    <input                     
+                    type="text"
+                    name='description'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                     />                    
+                </form>
+                    <EachCategory  ></EachCategory>
+
                 <div className='bttnSubCancel'>
                     <button className='bttn-cancel'>Cancel</button> 
-                    <button className='bttn-submit'>Submit</button>                     
+                    <button className='bttn-submit' >Submit</button>                     
                 </div>
                 
 
