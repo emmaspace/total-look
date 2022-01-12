ad6aa803e31edd4737bd7db5126e071b65becd52
Nancy <nancy.090495@gmail.com>
2022-01-12 10:21:41 -0600

    first changes to header

---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "./Header";
+
+export const Welcome = () => {
+    return (
+        <section>
+            <Header />
+            
+        </section>
+    )
+}
+
+export default Welcome;
