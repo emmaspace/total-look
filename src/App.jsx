326a97a3d04a924b508d35fb1cd51df40f34e357
Emma <emmarraguila@comunidad.unam.mx>
2022-01-12 11:45:48 -0600

    fix: conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import './App.css';
 import Home from './Components/Home/Home';
 
 
